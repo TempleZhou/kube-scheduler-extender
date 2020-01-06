2190124116b3b651780f9b87b6052053753e98f8
Temple Zhou <dbaztp@gmail.com>
2020-01-06 16:40:36 +0800

    删除 list namespace 逻辑

--- detecttoomanypodsinitializing/detect_too_many_pods_initializing.go
+++ detecttoomanypodsinitializing/detect_too_many_pods_initializing.go
@@ -5,40 +5,29 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"kube-scheduler-extender/kubeclientset"
-	"strings"
 )
 
 func DetectTooManyPodsInitializing(node v1.Node, maxInitializingPods int) (res bool) {
 	clientset := kubeclientset.Client
-	namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
+	cnt := 0
+
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{FieldSelector: "spec.nodeName=" + node.Name})
 	if err != nil {
 		panic(err.Error())
 	}
-	cnt := 0
 
-	for _, item := range namespaces.Items {
-		namespace := item.Name
-		if strings.HasPrefix(namespace, "kube-") {
-			continue
-		}
-		pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{FieldSelector: "spec.nodeName=" + node.Name})
-		if err != nil {
-			panic(err.Error())
-		}
-
-		for _, podItem := range pods.Items {
-			podIsReady := false
-			for _, condition := range podItem.Status.Conditions {
-				if condition.Type == v1.PodReady &&
-					condition.Status == v1.ConditionTrue {
-					podIsReady = true
-				}
-
+	for _, podItem := range pods.Items {
+		podIsReady := false
+		for _, condition := range podItem.Status.Conditions {
+			if condition.Type == v1.PodReady &&
+				condition.Status == v1.ConditionTrue {
+				podIsReady = true
 			}
-			if !podIsReady {
-				log.Info("pod ", podItem.Name, " on node:", podItem.Status.HostIP, " is not ready...")
-				cnt++
-			}
+
+		}
+		if !podIsReady {
+			log.Info("pod ", podItem.Name, " on node:", podItem.Status.HostIP, " is not ready...")
+			cnt++
 		}
 	}
 
